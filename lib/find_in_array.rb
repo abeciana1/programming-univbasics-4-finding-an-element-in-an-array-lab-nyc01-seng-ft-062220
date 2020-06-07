af528c9f812d1a7a292a1ad1ff1a3803e92297ff
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 21:35:36 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -9,4 +9,8 @@ def find_element_index(array, value_to_find)
     array[count] == value_to_find
     count += 1 
   end
+  
+  if array[count] == value_to_find do
+    return array.index(value_to_find)
+  end
 end
