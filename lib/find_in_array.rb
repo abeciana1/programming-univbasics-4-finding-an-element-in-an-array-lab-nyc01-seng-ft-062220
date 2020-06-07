c44bf87c06508d4641a6bc640b03883a4b905085
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 21:24:01 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,8 @@ def find_element_index(array, value_to_find)
   not_found = nil
   
   if array.include?(value_to_find) == true do
-    
+    return array.index(value_to_find)
   end
+  elseif puts not_found
   
 end
