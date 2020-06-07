76e61be87b21928b07eaad95e57328afba57d9f1
Alex Beciana <alex.beciana@gmail.com>
2020-06-07 21:44:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,13 +3,6 @@
 #end
 
 def find_element_index(array, value_to_find)
-  count = 0 
-  
-  #while array[count] do
-   # array[count] == value_to_find
-    #count += 1 
-#  end
-  
   if array.include?(value_to_find) == true
     array.index(value_to_find)
   end
